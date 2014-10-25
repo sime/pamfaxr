c4c16aff538574fcb5eb69905677ad56677edebb
Simon Males <sime@sime.net.au>
2014-10-25 13:27:10 +0200

    No need to autorun anymore

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,6 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'pamfaxr'
 require 'rspec'
-require 'rspec/autorun'
 require 'fakeweb'
 require 'json'
 
